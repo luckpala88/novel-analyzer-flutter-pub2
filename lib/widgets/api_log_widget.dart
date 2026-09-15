66661b0f6014305b4783d8df8d7a1523834d9978
luckpala <luckpala@users.noreply.github.com>
2026-09-16 02:42:07 +0800

    snap v546

---
+++ lib/widgets/api_log_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// API日志组件（占位，实际日志在ScanPage内联实现）
+class ApiLogWidget extends StatefulWidget {
+  const ApiLogWidget({super.key});
+
+  @override
+  State<ApiLogWidget> createState() => _ApiLogWidgetState();
+}
+
+class _ApiLogWidgetState extends State<ApiLogWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
