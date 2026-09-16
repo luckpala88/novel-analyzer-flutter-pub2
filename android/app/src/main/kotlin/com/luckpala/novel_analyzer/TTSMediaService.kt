f6bcdb38dc235acb0d333e156a96a97a62818c0d
luckpala <luckpala@users.noreply.github.com>
2026-09-16 16:46:25 +0800

    snap v559

--- android/app/src/main/kotlin/com/luckpala/novel_analyzer/TTSMediaService.kt
+++ android/app/src/main/kotlin/com/luckpala/novel_analyzer/TTSMediaService.kt
@@ -212,7 +212,7 @@ class TTSMediaService : MediaBrowserServiceCompat() {
 
     override fun onLoadChildren(
         parentId: String,
-        result: Result<MutableList<android.media.browse.MediaBrowser.MediaItem>>
+        result: MediaBrowserServiceCompat.Result<MutableList<android.media.browse.MediaBrowser.MediaItem>>
     ) {
         result.sendResult(mutableListOf())
     }
